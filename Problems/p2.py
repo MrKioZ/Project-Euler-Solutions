ec83e8a3052149162819c0d4499f04072e4ff46f
Mr.Kio <38723412+MrKioZ@users.noreply.github.com>
2019-12-26 20:08:14 +0200

    Update p2.py

--- Problems/p2.py
+++ Problems/p2.py
@@ -1,1 +1,6 @@
+"""
+I is a Fibonacci number if and only if 5 I**2 + 4 or 5 I**2 – 4 is a square number.
+3 is a Fibonacci number since 5x3**2+4 is 49 which is 7**2
+5 is a Fibonacci number since 5x5**2–4 is 121 which is 11**2
+"""
 print(sum([i for i in range(1,int(4000000)) if ((((5*(i**2)+4)**.5).is_integer()) or (((5*(i**2)-4)**.5).is_integer())) and (i%2==0)]))
