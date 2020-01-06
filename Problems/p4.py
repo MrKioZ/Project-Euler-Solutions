1f243add18ffec5774675af758af117ffe5d2faa
Mr.Kio <38723412+MrKioZ@users.noreply.github.com>
2020-01-06 09:14:06 +0200

    Update p4.py

--- Problems/p4.py
+++ Problems/p4.py
@@ -4,4 +4,4 @@ A palindromic number is a number that remains the same when its digits are rever
 This code was intentionally made in single line as a challenge not for the runtime
 """
 
-print(max(a*b for a in range(9999, 100, -1) for b in range(a, 100, -1) if str(a*b) == str(a*b)[::-1]))
+print(max(i*e for i in range(1000,2,-1) for e in reversed(range(2,1000)) if (str(i*e)[::-1] == str(i*e)) and ((len(str(i)) + len(str(e))) == 6)))
