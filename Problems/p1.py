e92f468c98e32252a10c8925d87734e786393bea
Mr.Kio <38723412+MrKioZ@users.noreply.github.com>
2020-01-06 09:27:30 +0200

    Update p1.py

--- Problems/p1.py
+++ Problems/p1.py
@@ -1,7 +1,1 @@
-sum = 0
-
-for i in range(1, 1000):
-    if ((i/3).is_integer() or (i/5).is_integer()):
-        sum += i
-
-print(sum)
+print(sum(i for i in range(1,1000) if (i%3 == 0) or (i%5 == 0)))
