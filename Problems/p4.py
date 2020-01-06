c8f54da76fbd8161de8f183963327f1a8c2a72e0
Mr.Kio <38723412+MrKioZ@users.noreply.github.com>
2020-01-06 09:18:04 +0200

    Update p4.py

--- Problems/p4.py
+++ Problems/p4.py
@@ -5,3 +5,5 @@ This code was intentionally made in single line as a challenge not for the runti
 """
 
 print(max(i*e for i in range(1000,2,-1) for e in reversed(range(2,1000)) if (str(i*e)[::-1] == str(i*e)) and ((len(str(i)) + len(str(e))) == 6)))
+
+# output -> 906609
