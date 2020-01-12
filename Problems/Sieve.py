caf3a429876a19d81348d620cd9d9f3c6ca3d400
Mr.Kio <38723412+MrKioZ@users.noreply.github.com>
2020-01-12 17:54:52 +0200

    Create Sieve.py

---
+++ Problems/Sieve.py
@@ -0,0 +1,99 @@
+def SieveOfAtkin(limit):
+
+    # 2 and 3 are known
+    # to be prime
+    if (limit > 2):
+        print(2 , end = " ")
+    if (limit > 3):
+        print(3 , end = " ")
+
+    # Initialise the sieve
+    # array with False values
+    sieve = [False] * limit
+    for i in range( 0 , limit ):
+        sieve[i] = False
+
+    '''Mark siev[n] is True if
+    one of the following is True:
+    a) n = (4*x*x)+(y*y) has odd
+    number of solutions, i.e.,
+    there exist odd number of
+    distinct pairs (x, y) that
+    satisfy the equation and
+    n % 12 = 1 or n % 12 = 5.
+    b) n = (3*x*x)+(y*y) has
+    odd number of solutions
+    and n % 12 = 7
+    c) n = (3*x*x)-(y*y) has
+    odd number of solutions,
+    x > y and n % 12 = 11 '''
+    x = 1
+    while(x * x < limit ) :
+        y = 1
+        while(y * y < limit ) :
+
+            # Main part of
+            # Sieve of Atkin
+            n = (4 * x * x) + (y * y)
+            if (n <= limit and (n % 12 == 1 or
+                                n % 12 == 5)):
+                sieve[n] ^= True
+
+            n = (3 * x * x) + (y * y)
+            if (n <= limit and n % 12 == 7):
+                sieve[n] ^= True
+
+            n = (3 * x * x) - (y * y)
+            if (x > y and n <= limit and
+                          n % 12 == 11):
+                sieve[n] ^= True
+            y += 1
+        x += 1
+
+    # Mark all multiples of
+    # squares as non-prime
+    r = 5
+    while(r * r < limit) :
+        if (sieve[r]) :
+            for i in range(r * r, limit, r * r):
+                sieve[i] = False
+
+    # Print primes
+    # using sieve[]
+    for a in range(5 , limit ):
+        if (sieve[a]):
+            print(a , end = " ")
+
+
+def SieveOfEratosthenes(n):
+
+    # Create a boolean array "prime[0..n]" and initialize
+    #  all entries it as true. A value in prime[i] will
+    # finally be false if i is Not a prime, else true.
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+
+        # If prime[p] is not changed, then it is a prime
+        if (prime[p] == True):
+
+            # Update all multiples of p
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+
+    # Print all prime numbers
+    for p in range(2, n):
+        if prime[p]:
+            print p,
+
+
+if __name__=='__main__':
+
+    limit = 20
+    SieveOfAtkin(limit)
+
+    n = 30
+    print("Following are the prime numbers smaller")
+    print("than or equal to", n)
+    SieveOfEratosthenes(n)
